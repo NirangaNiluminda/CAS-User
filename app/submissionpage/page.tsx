cc8c73a0b6e00c63761c9a22772f692f88819733
SandeepaMuthukumari <166691172+SandeepaMuthukumari@users.noreply.github.com>
2024-08-11 16:49:16 +0530

    Add submission page

---
+++ app/submissionpage/page.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import React from 'react';
+
+const SubmissionPage: React.FC = () => {
+
+    const router = useRouter();
+
+    const handleGoBack = () => {
+        router.push('/question12');
+    };
+
+    const handleSubmit = () => {
+        router.push('/scorepage');
+    };
+
+    return (
+        <div className="w-screen h-screen flex flex-col items-center bg-white">
+            <div className="w-full h-[113px] bg-[#d9d9d9] flex items-center justify-center">
+                <div className="text-black text-[40px] font-bold">Submission</div>
+            </div>
+            <div className="w-[90%] h-auto mt-5 bg-[#d9d9d9]/40 rounded-[11px] grid grid-cols-10 gap-2 justify-items-center items-start">
+                {Array.from({ length: 20 }, (_, i) => (
+                    <div
+                        key={i}
+                        className="px-5 py-2.5 m-2 rounded-md border-2 border-[#0cdc09] flex flex-col items-center"
+                        style={{
+                            width: '84px', // fixed width
+                            height: '84px', // fixed height
+                        }}
+                    >
+                        <div className="text-center text-black text-[32px] font-bold">{i + 1}</div>
+                        <div className="w-full h-20 bg-[#0cdc09] rounded-md mt-2" />
+                    </div>
+                ))}
+            </div>
+            <div className="w-[90%] mt-auto mb-10 flex justify-between">
+                <button
+                    type="button"
+                    className="focus:outline-none text-black bg-[#0cdc09] hover:bg-green-800 hover:border hover:border-[#0cdc09] focus:ring-4 focus:ring-green-300 font-bold font-['Inter'] tracking-[3.60px] rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-[#0cdc09] dark:hover:bg-transparent dark:focus:ring-green-800 transform transition-transform duration-300 hover:scale-x-110"
+                    onClick={handleGoBack}
+                >
+                    Go Back
+                </button>
+                <button
+                    type="button"
+                    className="focus:outline-none text-black bg-[#0cdc09] hover:bg-green-800 hover:border hover:border-[#0cdc09] focus:ring-4 focus:ring-green-300 font-bold font-['Inter'] tracking-[3.60px] rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-[#0cdc09] dark:hover:bg-transparent dark:focus:ring-green-800 transform transition-transform duration-300 hover:scale-x-110"
+                    onClick={handleSubmit}
+                >
+                    Submit All
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default SubmissionPage;
