2f48a649de9e12956880221062f75c3f9667f12c
G.W.Niranga Niluminda <145843409+NirangaNiluminda@users.noreply.github.com>
2024-10-01 11:49:45 +0530

    update the URL

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-
+require("dotenv").config();
 import { useRouter } from 'next/navigation'
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -27,7 +27,7 @@ const SignUp: React.FC = () => {
       if (window.location.hostname === 'localhost') {
         setApiUrl('http://localhost:4000');
       } else {
-        setApiUrl('http://3.26.208.29');
+        setApiUrl(`${process.env.AWS_URL}`);
       }
     }
   }, []);
