eac85c4dce49c84488db94f1f89f3338efa675c7
Kavindu Dhananjaya <rathnayaka_rmkd_e23@engug.ruh.ac.lk>
2024-10-09 18:29:47 +0530

    letting student to sign up or sign in when navigating to quiz

---
+++ app/signin/[id]/page.tsx
@@ -0,0 +1,118 @@
+'use client'
+require("dotenv").config();
+import { useParams, useRouter } from 'next/navigation'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useUser } from '@/context/UserContext';
+import Image from 'next/image';
+
+
+const SignIn = () => {
+    const router = useRouter();
+    const { id } = useParams();
+
+    const [formData, setFormData] = useState({
+        registrationNumber: '',
+        password: '',
+    })
+
+    const [rememberMe, setRememberMe] = useState(false);
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFormData({ ...formData, [e.target.id]: e.target.value })
+    }
+
+    const handleRememberMeChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setRememberMe(e.target.checked);
+    }
+
+    const {setUser} = useUser();
+    const [apiUrl, setApiUrl] = useState('');
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      // This will run only on the client side
+      if (window.location.hostname === 'localhost') {
+        setApiUrl('http://localhost:4000');
+      } else {
+        setApiUrl('http://3.26.208.29');
+      }
+    }
+  }, []);
+    const handleSignIn = async () => {
+        try{
+            const response = await axios.post(`${apiUrl}/api/v1/login-user`,{
+                registrationNumber: formData.registrationNumber,
+                password: formData.password,
+            })
+
+            if(response.status === 200 || response.data.success){
+                const token = response.data.accessToken; // accesstoken
+                sessionStorage.setItem('name', response.data.user.name);
+                if (rememberMe) {
+                    localStorage.setItem('token', token);
+                } else {
+                    sessionStorage.setItem('token', token);
+                }
+                setUser(response.data.user)
+                router.push(`/modulepage/${id}`);
+            }
+            else{
+                alert('Invalid credentials')
+            }
+        }
+        catch(error){
+            console.error('Error during sign in:', error);
+            alert(`An error occurred. Please try again. ${error}`);
+        }
+    }
+
+    return (
+        <div className='w-full h-screen flex justify-center items-center'>
+            <div className="w-[830px] h-[640px] flex flex-col justify-center items-center gap-[42px]">
+                <div className="self-stretch h-[25px] text-center text-black text-[32px] font-bold font-['Inter']">Sign in</div>
+                <Image className="w-[273px] h-60" src="/SignIn.png" alt='sign in image' width={380} height={380}  />
+                <div className="self-stretch flex flex-col justify-center items-center gap-[20px]">
+                    <div className="flex gap-[86px]">
+                        <div className="h-[68px] relative">
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Index</label>
+                            <input type="text" id="registrationNumber" value={formData.registrationNumber} onChange={handleChange} className="bg-green-200 border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 placeholder-black" placeholder="EG/XXXX/XXXX" />
+                        </div>
+                        <div className="h-[68px] relative">
+                            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                            <input type="password" id="password" value={formData.password} onChange={handleChange} className="bg-green-200 border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 placeholder-black" placeholder="Password" />
+                        </div>
+                    </div>
+                    <div className="flex justify-between items-center w-full mt-4">
+                        <div className="flex items-center">
+                            <input id="rememberMe" type="radio" checked={rememberMe} onChange={handleRememberMeChange} name="default-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600" />
+                            <label className="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember Me</label>
+                        </div>
+                        <div className="flex items-center">
+                            <div className="w-[169px] h-4 text-black text-xl font-medium font-['Inter'] cursor-pointer">Forgot Password ?</div>
+                        </div>
+                    </div>
+                </div>
+                <button
+                    type="button"
+                    onClick={handleSignIn}
+                    className="focus:outline-none text-black bg-[#0cdc09] hover:bg-green-800 hover:border hover:border-[#0cdc09] focus:ring-4 focus:ring-green-300 font-bold font-['Inter'] tracking-[3.60px] rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-[#0cdc09] dark:hover:bg-transparent dark:focus:ring-green-800 transform transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-lg"
+
+                >
+                    Sign in
+                </button>
+                <div className="flex flex-row items-center gap-2">
+                    <div className="text-black text-xl font-light font-['Inter']">Don&apos;t have an account?</div>
+                    <button
+                        onClick={() => router.push(`/signup/${id}`)}
+                        className="text-blue-500 hover:underline text-xl font-bold font-['Inter']"
+                    >
+                        Sign up
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default SignIn;
---
+++ app/signup/[id]/page.tsx
@@ -0,0 +1,127 @@
+'use client'
+
+import { useParams, useRouter } from 'next/navigation'
+
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const SignUp: React.FC = () => {
+
+  const router = useRouter();
+  const { id } = useParams();
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    registrationNumber: '',
+    password: '',
+    confirmPassword: '',
+  })
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value })
+  }
+  const [apiUrl, setApiUrl] = useState('');
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      // This will run only on the client side
+      if (window.location.hostname === 'localhost') {
+        setApiUrl('http://localhost:4000');
+      } else {
+        setApiUrl(`${process.env.AWS_URL}`);
+      }
+    }
+  }, []);
+  const handleSignUp = async () => {
+    // console.log({
+    //   name: formData.name,
+    //   email: formData.email,
+    //   password: formData.password,
+    //   registrationNumber: formData.registrationNumber,
+    // });
+    if (formData.password !== formData.confirmPassword) {
+      alert('Passwords do not match')
+      return
+    }
+
+    try {
+      const response = await axios.post(`${apiUrl}/api/v1/registration`, {
+        name: formData.name,
+        email: formData.email,
+        password: formData.password,
+        registrationNumber: formData.registrationNumber,
+      });
+
+      if (response.status === 201 || response.data.success) {
+        if (response.data.activationToken) {
+            const token = response.data.activationToken;
+            alert(`Registration successful! Please check your email to activate your account.`);
+            router.push(`/activate?token=${token}`);
+        } else {
+            // Registration successful, and no activation needed
+            router.push(`/signin/${id}`);
+        }
+      } else {
+        // Handle error response
+        alert('Registration failed. Please try again.');
+      }
+    } catch (error) {
+      // Handle error
+      console.error('Error during registration:', error);
+      alert('An error occurred. Please try again.');
+    }
+  }
+
+  return (
+    <div className='w-full h-screen flex justify-center items-center'>
+      <div className="w-[372px] h-[635px] flex flex-col justify-start items-center gap-[27px]">
+        <div className="self-stretch h-7 text-center text-black text-[32px] font-bold font-['Inter']">Signing you UP!</div>
+
+        <div className="h-[68px] relative w-full">
+          <label htmlFor="index" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Index</label>
+          <input type="text" id="registrationNumber" value={formData.registrationNumber} onChange={handleChange} className="bg-green-200 border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 placeholder-black" placeholder="EG/XXXX/XXXX" />
+        </div>
+
+        <div className="h-[68px] relative w-full">
+          <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
+          <input type="text" id="name" value={formData.name} onChange={handleChange} className="bg-green-200 border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 placeholder-black" placeholder="Perera A.B.C." />
+        </div>
+
+        <div className="h-[68px] relative w-full">
+          <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+          <input type="email" id="email" value={formData.email} onChange={handleChange} className="bg-green-200 border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 placeholder-black" placeholder="name@flowbite.com" />
+        </div>
+
+        <div className="h-[68px] relative w-full">
+          <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+          <input type="password" id="password" value={formData.password} onChange={handleChange} className="bg-green-200 border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 placeholder-black" placeholder="Password" />
+        </div>
+
+        <div className="h-[68px] relative w-full">
+          <label htmlFor="confirm-password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm Password</label>
+          <input type="password" id="confirmPassword" value={formData.confirmPassword} onChange={handleChange} className="bg-green-200 border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 placeholder-black" placeholder="Confirm Password" />
+        </div>
+
+        <button
+          type="button"
+          onClick={handleSignUp}
+          className="focus:outline-none text-black bg-[#0cdc09] hover:bg-green-800 hover:border hover:border-[#0cdc09] focus:ring-4 focus:ring-green-300 font-bold font-['Inter'] tracking-[3.60px] rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-[#0cdc09] dark:hover:bg-transparent dark:focus:ring-green-800 transform transition-all duration-300 ease-in-out hover:scale-105 hover:shadow-lg"
+        >
+          Sign up
+        </button>
+
+        <div className="flex flex-row items-center gap-2">
+          <div className="text-black text-xl font-light font-['Inter']">Already have an account?</div>
+          <button
+            onClick={() => router.push(`/signin/${id}`)}
+            className="text-blue-500 hover:underline text-xl font-bold font-['Inter']"
+          >
+            Sign in
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUp;
