bb942dac588427ee8846b36e35b70d1d1e2265e5
NirangaNiluminda <niranganiluminda@gmail.com>
2025-01-05 20:01:57 +0530

    chaneg the aws url

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -34,7 +34,7 @@ const SignIn = () => {
       if (window.location.hostname === 'localhost') {
         setApiUrl('http://localhost:4000');
       } else {
-        setApiUrl('http://3.26.208.29');
+        setApiUrl('http://13.236.193.185');
       }
     }
   }, []);
