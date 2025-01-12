f6a77d3e9d180895cee60da4952249acb6b45c22
KavinduDr <rmkavindudhananjaya@gmail.com>
2025-01-12 06:59:47 +0530

    Add navigation button to confirm correct answers and redirect to completed page

--- app/correct_answers/[id]/page.tsx
+++ app/correct_answers/[id]/page.tsx
@@ -1,12 +1,13 @@
 'use client'
 import { useQuiz } from '@/context/QuizContext'
-import { useParams } from 'next/navigation'
+import { useParams, useRouter } from 'next/navigation'
 import React, { useEffect, useState } from 'react'
 
 const page = () => {
 
   const { quiz, setQuiz } = useQuiz();
   const { id } = useParams();
+  const router = useRouter();
 
   interface QuizData {
     assignment: {
@@ -56,11 +57,13 @@ const page = () => {
   }, [id]);
 
   return (
-    <div className="container mx-auto p-4">
+    <div className="container mx-auto p-4 flex justify-center">
       {quizData ? (
         <div className="mb-4">
-          <h2 className="text-2xl font-bold mb-4">Correct Answers for: {quizData?.assignment.title}</h2>
-          <div className='space-y-4'>
+          <div className='flex w-full h-[113px] justify-center items-center'>
+            <h2 className="text-2xl md:text-3xl font-bold mb-4">Correct Answers for: {quizData?.assignment.title}</h2>
+          </div>
+          <div className='space-y-4 mt-5'>
             {quizData?.assignment.questions.map((question) => (
               <div key={question?._id}>
                 <h3>Question: {question?.questionText}</h3>
@@ -73,6 +76,15 @@ const page = () => {
                 </ul>
               </div>
             ))}
+          </div>
+          <div className='flex justify-center mt-5'>
+            <button
+              type="button"
+              className="focus:outline-none text-black bg-[#0cdc09] hover:bg-green-800 hover:border hover:border-[#0cdc09] focus:ring-4 focus:ring-green-300 font-bold font-['Inter'] tracking-[3.60px] rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-[#0cdc09] dark:hover:bg-transparent dark:focus:ring-green-800 transform transition-transform duration-300 hover:scale-x-110"
+              onClick={() => router.push('/completed')}
+            >
+              Confirm
+            </button>
           </div>
         </div>
       ) : (
